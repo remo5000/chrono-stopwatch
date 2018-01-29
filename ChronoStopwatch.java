35117b71aa5fa4f0364a2f69d14273422b136f76
remo5000 <vigneshshankar@gmail.com>
2018-01-29 19:54:26 +0800

    clean up comments

--- ChronoStopwatch.java
+++ ChronoStopwatch.java
@@ -1,4 +1,4 @@
-package com.example.vignesh.vigneshtestinggrounds;
+package /* Insert package name here */
 
 import android.content.Context;
 import android.os.SystemClock;
@@ -6,13 +6,13 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.Chronometer;
 
-/**A stopwatch chronometer that can start,stop,reset and save(soon to be) the time!
+/**
+ * A stopwatch chronometer that can start,stop and reset.
  * Create an object on your activity code and use the methods here.
  * Created by remo5000 on 2/8/2017.
  */
 
 public class ChronoStopwatch extends Chronometer {
-
     long ChronoStopwatchTimeElapsedSincePause;
     boolean ChronoStopwatchTimerIsRunning;
 
@@ -20,7 +20,6 @@ public class ChronoStopwatch extends Chronometer {
         super(context, null, 0);
         init();
     }
-
 
     public ChronoStopwatch(Context context, AttributeSet attrs) {
         super(context, attrs, 0);
@@ -40,7 +39,8 @@ public class ChronoStopwatch extends Chronometer {
     public void init(){
         this.setBase(SystemClock.elapsedRealtime());
         ChronoStopwatchTimerIsRunning = false;
-        ChronoStopwatchTimeElapsedSincePause = -1;                //time elapsed cannot be -1, therefore it means that timer has not started yet
+        // -1 used to represent an undefined time elapsed
+        ChronoStopwatchTimeElapsedSincePause = -1;
     }
 
     public void ResetChronometer(){
@@ -54,21 +54,23 @@ public class ChronoStopwatch extends Chronometer {
 
     public void StartOrStopChronometer(){
         if (!ChronoStopwatchTimerIsRunning) {
-            if (ChronoStopwatchTimeElapsedSincePause == -1) {                               //if timer is starting for the first time,
+            if (ChronoStopwatchTimeElapsedSincePause == -1) {
                 this.setBase(SystemClock.elapsedRealtime());
                 Log.d("StartOrStopChronometer", "Initial value is " + String.valueOf(this.getBase()));
                 this.start();
-            } else {                                                         //if resuming,
+            } else { 
                 this.setBase(SystemClock.elapsedRealtime() - (ChronoStopwatchTimeElapsedSincePause - this.getBase()));
-                Log.d("StartOrStopChronometer", "Value on resuming is " + String.valueOf(this.getBase()) + " and the current timer time is " + String.valueOf(SystemClock.elapsedRealtime() - this.getBase()));
+                Log.d("StartOrStopChronometer", "Value on resuming is " + String.valueOf(this.getBase()) 
+                    + " and the current timer time elapsed is " + String.valueOf(SystemClock.elapsedRealtime() - this.getBase()));
                 this.start();
             }
             ChronoStopwatchTimerIsRunning = true;
-        } else {                                                                    // if timer pausing,
+        } else {
+            // puase the stopwatch
             ChronoStopwatchTimeElapsedSincePause = SystemClock.elapsedRealtime();
             Log.d("StartOrStopChronometer", "Paused value is " + String.valueOf(ChronoStopwatchTimeElapsedSincePause));
             this.stop();
-                ChronoStopwatchTimerIsRunning = false;
-            }
+            ChronoStopwatchTimerIsRunning = false;
+        }
     }
 }
